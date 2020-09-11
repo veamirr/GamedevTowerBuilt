d9bb81d3da651609ea763d42903049a2dc65da6f
Veamirr <veamirr@gmail.com>
2020-09-12 01:40:01 +0300

    init
    init

--- Assets/Scripts/ScreenSwitcher.cs
+++ Assets/Scripts/ScreenSwitcher.cs
@@ -5,12 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class ScreenSwitcher : MonoBehaviour
 {
-    public void playGame()
+    public void playGame()//из стартового меню в игру
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);//к текущему адресы сцены +1, и загружаем следующую сцену
+    }
+    public void pauseGame()//остановить игру
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-    public void Back()
+    public void resumeGame()//продолжить игру
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void openGame()//вернуться в стартовое меню
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
     }
 }
