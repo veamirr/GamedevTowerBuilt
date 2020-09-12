a7f6d45ffd2b0bb50579d9cd4ac4994b3104d05d
Veamirr <veamirr@gmail.com>
2020-09-12 21:48:06 +0300

    12.09
    added scenes, transition buttons, BoxSpawner, moving and falling box

---
+++ Assets/Scripts/BoxScript.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxScript : MonoBehaviour
+{
+    private float min_x = -3.1f, max_x = 3.1f;
+
+    private bool canMove;
+    private float move_Speed = 2f;
+
+    private Rigidbody2D myBody;
+
+    private bool gameOver;
+    private bool ignoreCollision;
+    private bool ignoreTrigger;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        myBody.gravityScale = 0f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canMove = true;
+
+        if(Random.Range(0,2)>0)
+        {
+            move_Speed *= -1f;
+        }
+
+        GameplayController.instance.currentBox = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveBox();
+    }
+
+    void MoveBox()
+    {
+        if(canMove)
+        {
+            Vector3 temp = transform.position;
+            temp.x += move_Speed * Time.deltaTime;
+
+            if(temp.x > max_x)
+            {
+                move_Speed *= -1f;
+            }
+            else if (temp.x < min_x)
+            {
+                move_Speed *= -1f;
+            }
+
+            transform.position = temp;
+        }
+    }
+
+    public void DropBox()
+    {
+        canMove = false;
+        myBody.gravityScale = Random.Range(2, 4);
+    }
+
+    void Landed()
+    {
+        if (gameOver) return;
+
+        ignoreCollision = true;
+        ignoreTrigger = true;
+
+        GameplayController.instance.SpawnNewBox();
+        GameplayController.instance.MoveCamera();
+    }
+
+    void RestartGame()
+    {
+        GameplayController.instance.RestartGame();
+    }
+
+    void OnCollisionEnter2D(Collision2D target)
+    {
+        if (ignoreCollision) return;
+
+        if (target.gameObject.tag == "Platform")
+        {
+            Invoke("landed", 2f);
+            ignoreCollision = true;
+        }
+
+        if (target.gameObject.tag == "Box")
+        {
+            Invoke("landed", 2f);
+            ignoreCollision = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (ignoreTrigger) return;
+
+        if (target.tag == "GameOver")
+        {
+            CancelInvoke("Landed");
+            gameOver = true;
+            ignoreTrigger = true;
+
+            Invoke("RestartGame", 2f);
+
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/GameplayController.cs
@@ -0,0 +1,70 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayController : MonoBehaviour
+{
+    public static GameplayController instance;
+
+    public BoxSpawner box_Spawner;
+
+    public BoxScript currentBox;
+
+    public CameraFollow cameraScript;
+    private int moveCount;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        box_Spawner.SpawnBox();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectInput();
+    }
+
+    void DetectInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentBox.DropBox();
+        }
+    }
+
+    public void SpawnNewBox()
+    {
+        Invoke("NewBox", 2f);
+    }
+
+    void NewBox()
+    {
+        box_Spawner.SpawnBox();
+    }
+
+    public void MoveCamera()
+    {
+        moveCount++;
+
+        if (moveCount == 3)
+        {
+            moveCount = 0;
+            //cameraScript.targetPos.y += 2f;
+        }
+    }
+
+    public void RestartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+}
